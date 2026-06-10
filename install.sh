5cf0ddedcbf43832e3fb09f4fb642205c039676b
AHASANUL HAQUE AKASH <orewaakash0@gmail.com>
2026-06-10 18:21:14 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,15 @@ echo "[+] Installing Banner..."
 cp banner.sh $HOME/
 chmod +x $HOME/banner.sh
 
+# auto run via login shell (BEST WAY)
+mkdir -p $PREFIX/etc/profile.d
+
+cat > $PREFIX/etc/profile.d/banner.sh << 'EOF'
+clear
+bash $HOME/banner.sh
+EOF
+
+chmod +x $PREFIX/etc/profile.d/banner.sh
+
 echo "[✓] Done!"
-echo "[✓] Run manually: bash ~/banner.sh"
+echo "[✓] Now just reopen Termux"
